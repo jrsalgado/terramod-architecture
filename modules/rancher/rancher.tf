7182f15908323efbf243aecd1b3ac35198eeb06d
Jayro Salgado <jsalgado@NSL-JSALGADOs-MacBook-Pro.local>
2018-02-24 13:22:29 -0700

    add route 53 from catalog

--- modules/rancher/rancher.tf
+++ modules/rancher/rancher.tf
@@ -15,115 +15,106 @@ resource "rancher_registration_token" "default" {
   environment_id = "${data.rancher_environment.default.id}"
 }
 
-# resource "rancher_stack" "letsChat" {
-#   name            = "lets-chat-2"
-#   description     = "This is a chat app"
-#   environment_id  = "${data.rancher_environment.default.id}"
-#   start_on_create = 1
-#   finish_upgrade  = 1
-#   docker_compose  =  <<EOF
-# version: '2'
-# services:
-#   database:
-#     image: mongo
-#     stdin_open: true
-#     tty: true
-#     labels:
-#       io.rancher.container.pull_image: always
-#   website:
-#     image: rancher/lb-service-haproxy:v0.7.5
-#     ports:
-#     - 80:80/tcp
-#     labels:
-#       io.rancher.container.agent.role: environmentAdmin
-#       io.rancher.container.create_agent: 'true'
-#       io.rancher.scheduler.global: 'true'
-#   web:
-#     image: sdelements/lets-chat
-#     stdin_open: true
-#     tty: true
-#     links:
-#     - database:mongo
-#     labels:
-#       io.rancher.container.pull_image: always
-# EOF
+resource "rancher_stack" "letsChat-5" {
+  name            = "lets-chat-5"
+  description     = "This is a chat app"
+  environment_id  = "${data.rancher_environment.default.id}"
+  start_on_create = 1
+  finish_upgrade  = 1
+  docker_compose  =  <<EOF
+version: '2'
+services:
+  database:
+    image: mongo
+    stdin_open: true
+    tty: true
+    labels:
+      io.rancher.container.pull_image: always
+  website:
+    image: rancher/lb-service-haproxy:v0.7.5
+    ports:
+    - 80:80/tcp
+    labels:
+      io.rancher.container.agent.role: environmentAdmin
+      io.rancher.container.create_agent: 'true'
+      io.rancher.scheduler.global: 'true'
+  web:
+    image: sdelements/lets-chat
+    stdin_open: true
+    tty: true
+    links:
+    - database:mongo
+    labels:
+      io.rancher.container.pull_image: always
+EOF
 
-#   rancher_compose = <<EOF
-# version: '2'
-# services:
-#   database:
-#     scale: 1
-#     start_on_create: true
-#   website:
-#     start_on_create: true
-#     lb_config:
-#       certs: []
-#       port_rules:
-#       - hostname: ''
-#         priority: 1
-#         protocol: http
-#         service: web
-#         source_port: 80
-#         target_port: 8080
-#     health_check:
-#       response_timeout: 2000
-#       healthy_threshold: 2
-#       port: 42
-#       unhealthy_threshold: 3
-#       initializing_timeout: 60000
-#       interval: 2000
-#       reinitializing_timeout: 60000
-#   web:
-#     scale: 1
-#     start_on_create: true
-# EOF
+  rancher_compose = <<EOF
+version: '2'
+services:
+  database:
+    scale: 1
+    start_on_create: true
+    health_check:
+      response_timeout: 2000
+      healthy_threshold: 2
+      strategy: recreate
+      port: 27017
+      unhealthy_threshold: 3
+      initializing_timeout: 60000
+      interval: 2000
+      reinitializing_timeout: 60000
+  website:
+    start_on_create: true
+    lb_config:
+      certs: []
+      port_rules:
+      - hostname: ''
+        priority: 1
+        protocol: http
+        service: web
+        source_port: 80
+        target_port: 8080
+    health_check:
+      response_timeout: 2000
+      healthy_threshold: 2
+      port: 42
+      unhealthy_threshold: 3
+      strategy: recreate
+      initializing_timeout: 60000
+      interval: 2000
+      reinitializing_timeout: 60000
+  web:
+    scale: 1
+    start_on_create: true
+    health_check:
+      response_timeout: 2000
+      healthy_threshold: 2
+      strategy: recreate
+      port: 8080
+      unhealthy_threshold: 3
+      initializing_timeout: 60000
+      interval: 2000
+      reinitializing_timeout: 60000
+EOF
 
-# }
+}
 
-# # TODO: Add Route53
-# resource "rancher_stack" "Route53" {
-#   name            = "route53-1"
-#   description     = "external dns"
-#   environment_id  = "${data.rancher_environment.default.id}"
-#   start_on_create = 0
-#   finish_upgrade  = 1
-#   docker_compose  =  <<EOF
-# version: '2'
-# services:
-#   route53:
-#     image: rancher/external-dns:v0.7.0
-#     environment:
-#       AWS_ACCESS_KEY: 
-#       AWS_SECRET_KEY: 
-#       NAME_TEMPLATE: '%{{service_name}}.%{{stack_name}}.%{{environment_name}}'
-#       ROOT_DOMAIN: 
-#       ROUTE53_ZONE_ID: 
-#       TTL: '60'
-#     volumes:
-#     - /var/lib/rancher:/var/lib/rancher
-#     expose:
-#     - '1000'
-#     command:
-#     - -provider=route53
-#     labels:
-#       io.rancher.container.agent.role: external-dns
-#       io.rancher.container.create_agent: 'true'
-# EOF
-#   rancher_compose = <<EOF
-# version: '2'
-# services:
-#   route53:
-#     scale: 1
-#     start_on_create: true
-#     health_check:
-#       healthy_threshold: 2
-#       response_timeout: 5000
-#       port: 1000
-#       unhealthy_threshold: 2
-#       interval: 10000
-#       strategy: recreate
-#       request_line: GET / HTTP/1.0
+resource "rancher_stack" "external-dns" {
+  name           = "route53"
+  description    = "Route53 stack"
+  environment_id = "${data.rancher_environment.default.id}"
+  catalog_id     = "library:route53:7"
+  scope          = "system"
+  start_on_create = 1
+  finish_upgrade  = 1
 
-# EOF
-
-# }
+  environment {
+    AWS_ACCESS_KEY        = ""
+    AWS_SECRET_KEY        = ""
+    AWS_REGION            = "us-west-1"
+    TTL                   = "60"
+    ROOT_DOMAIN           = ""
+    ROUTE53_ZONE_ID       = ""
+    HEALTH_CHECK_INTERVAL = "15"
+  }
+}
--- rancherConfScheduler.sh
+++ rancherConfScheduler.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Try 2 times to configure with a delay of 1 minute
 COUNT=0
-MAXRETRY=15
+MAXRETRY=20
 RETRYDELAY=60
 
 while [ $COUNT -lt $MAXRETRY ]; do
-# terraform apply -var-file=envs/test/terraform.tfvars -target=module. envs/test 2>/dev/null
-docker run -it --rm -v $PWD:/usr/terransible --env-file private/env.list  jrsalgado/terraform:latest apply -auto-approve -var-file=envs/test/terraform.tfvars -target=module.rancher envs/test
+docker run -it --rm -v $PWD:/usr/terransible --env-file private/env.list  jrsalgado/terraform:latest apply -auto-approve -var-file=envs/test/terraform.tfvars -target=module.rancher -target=module.rancher_hosts envs/test
+# docker run -it --rm -v $PWD:/usr/terransible --env-file private/env.list  jrsalgado/terraform:latest plan -var-file=envs/test/terraform.tfvars -target=module.rancher -target=module.rancher_hosts envs/test
+# docker run -it --rm -v $PWD:/usr/terransible --env-file private/env.list  jrsalgado/terraform:latest destroy -var-file=envs/test/terraform.tfvars -target=module.rancher -target=module.rancher_hosts envs/test
 if [ $? -gt 0 ]; then
     echo "Retry script in "
     COUNT=$[$COUNT+1]
